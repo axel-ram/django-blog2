c01a5f2a16641fe88e1451189288d67282f176f7
Ramnath Sharma <ramsharma.axel@gmail.com>
2020-09-15 12:08:50 +0530

    basic ui updated

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+posts = [
+	{
+		'author': 'Ramnath',
+		'title': 'How to create templated in django',
+		'content': 'Content is coming soon',
+		'date_posted': 'September 15, 2020'
+	},
+	{
+		'author': 'Tanmay',
+		'title': 'How to do competitive programming',
+		'content': 'Content will be upated soon',
+		'date_posted': 'September 11, 2020'
+	},
+]
 
 # Create your views here.
+def home(request):
+	context = {
+		'posts': posts
+	}
+	return render(request, 'blog/home.html', context)
+
+def about(request):
+	return render(request, 'blog/about.html', {'title': 'About'})
