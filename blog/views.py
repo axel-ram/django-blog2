6634c970ce8ecba1e2b7a5361ac4622e3b084945
Ramnath Sharma <ramsharma.axel@gmail.com>
2020-10-03 17:06:24 +0530

    password reset added

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.models import User
 from django.views.generic import (
 	ListView, 
 	DetailView,
@@ -7,6 +8,9 @@ from django.views.generic import (
 	DeleteView,
 )
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+
+
+
 
 from .models import Post
 # Create your views here.
@@ -23,6 +27,19 @@ class PostListView(ListView):
 	template_name = 'blog/home.html'
 	context_object_name = 'posts'
 	ordering = ['-date_posted']
+	paginate_by = 5
+
+class UserPostListView(ListView):
+	model = Post
+	# <app>/<model>_<viewtype.html>
+	template_name = 'blog/user_home.html'
+	context_object_name = 'posts'
+	ordering = ['-date_posted']
+	paginate_by = 5
+
+	def get_queryset(self):
+		user = get_object_or_404(User, username = self.kwargs.get('username'))
+		return Post.objects.filter(author=user).order_by('-date_posted')
 
 class PostDetailView(DetailView):
 	# <app>/<model>_<viewtype.html>
